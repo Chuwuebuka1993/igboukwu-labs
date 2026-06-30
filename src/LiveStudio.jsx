d2e6274235e5d778bb67316cdd07c05d238fc85f
Jerome Madubuike <jerome@igboukwulabs.com>
2026-06-30 20:45:44 +0100

    fix missing useCallback import

--- src/LiveStudio.jsx
+++ src/LiveStudio.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import * as Tone from "tone";
 import bronzePot from "./assets/branding/igbo_ukwu_pot.jpg";
 
